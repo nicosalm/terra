275693c67ead94520b28fe951a59c239c84d0df4
Nico Salm <nico@salm.dev>
2026-07-10 19:48:50 -0500

    add a (placeholder) main, update README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# genesis (wip)
+[![tests](https://github.com/nicosalm/genesis/actions/workflows/rust.yml/badge.svg)](https://github.com/nicosalm/terra/actions/workflows/rust.yml) [![Rust Edition](https://img.shields.io/badge/rust-2024-orange.svg)](https://doc.rust-lang.org/edition-guide/rust-2024/index.html)
+
 The goal of this project is to procedurally generate a configurable landmass. A terrain heightmap, plus a water layer, plus a number of other possible ideas:
 - biomes
 - civilizations
@@ -9,5 +12,5 @@ In theory once we have these static elements we can then layer in a time tick el
 - economy
 - environment / seasons
 - war
-- 
+-
 ...but these elements might be just a *little bit* complicated, so we'll see what ends up happening.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 mod grid;
+mod heightmap;
+
+use grid::Grid;
+use heightmap::{TerrainParams, heightmap};
+
+const RAMP: [char; 10] = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@'];
+
+fn to_symbols(grid: &Grid<f32>) -> String {
+    let mut out = String::with_capacity(grid.side_length * (grid.side_length * 2 + 1));
+    for y in 0..grid.side_length {
+        for x in 0..grid.side_length {
+            let v = grid.get(x, y).clamp(0.0, 1.0);
+            let idx = ((v * (RAMP.len() - 1) as f32).round() as usize).min(RAMP.len() - 1);
+            out.push(RAMP[idx]);
+            out.push(RAMP[idx]);
+        }
+        out.push('\n');
+    }
+    out
+}
 
 fn main() {
-    println!("Hello, world!");
+    let params = TerrainParams {
+        initial_amplitude: 1.0,
+        roughness: 0.6,
+        seed: 42,
+    };
+
+    let terrain = heightmap(65, &params);
+    print!("{}", to_symbols(&terrain));
 }
